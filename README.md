74dbac5ef8da72a4a061a9dfa86a48bd71048586
Sergio Meneses <sergio.meneses@globant.com>
2019-05-28 10:16:17 -0500

    Functions Structure implemented

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # vinstall
-Bash script to install Vim plugins
+Only a small Bash script to manage VIM-Plugins
+
+* Install Plugins: vinstall install GitURL PluginName
+* Update Plugins: vinstall update
+* Remove Plugins: vinstall remove PluginName
--- vinstall.sh
+++ vinstall.sh
@@ -1,12 +1,64 @@
 #/bin/bash
 # $1 git URL, $2 plugin-name
 # path ~/.vim/pack/myplugins/start/
-cd ~/.vim/pack/myplugins/start/
 
-git clone $1.git
-cd $1
+function validation(){
+	#echo "$(ls -lah ~/.$1)"
+	if [ -d "/$HOME/.$1/pack/myplugins/start" ] ; then
+		echo "The configuration folder is in place"
+		
+	else
+		echo "Preparing the environtment..."
+		mkdir ~/.$1
+		mkdir ~/.$1/pack/
+		mkdir ~/.$1/pack/myplugins/
+		mkdir ~/.$1/pack/myplugins/start/
+		echo "The configuration folder was configured properly!"
+	fi
 
-git submodule init
-git submodule add $1 vim/pack/start/$2
-git add .gitmodules vim/pack/start/$2
-git commit
+}
+
+function install(){
+	cd ~/.vim/pack/myplugins/start/
+	git clone $2
+	cd $2
+	git submodule init
+	git submodule add $2 vim/pack/start/$3
+	git add .gitmodules vim/pack/start/$3
+	git commit
+
+}
+
+function update(){
+	#git submodule update --remote --merge
+	#git commit
+	echo "update completed!..."
+}
+
+function remove(){
+	cd ~/.vim/pack/myplugins/start/$1
+	git submodule deinit vim/pack/start/$1
+	git rm vim/pack/start/$1
+	rm -Rf .git/modules/vim/pack/start/$1
+	git commit
+}
+
+validation "vim"
+
+case $1 in 
+	install)
+		echo "Installing plugin..."
+		install "$2 $3"
+		;;
+	update)
+		echo "Updating components..."
+		update
+		;;
+	remove)
+		echo "Removing plugin $2"
+		;;
+	*)
+		echo "Please insert a valid option [Install|Update|Remove]"
+		;;
+	esac
+
